4c4ec05e148e2c3b0e5bcd0bd388987b0d5d7d50
estherwanjiku007 <estherwanjiqu@gmail.com>
2024-02-29 13:54:20 +0300

    aster

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,10 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+    def walk(self):
+        print("The person is walking.")
+my_name=Person()
+my_name.talk()
+my_name.walk()
